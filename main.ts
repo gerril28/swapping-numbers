1e32974f3810a6f5c086a9daac25e8de80c77818
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-21 09:03:02 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(num2)
+    num2 += 1
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showNumber(num1)
+    num1 += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    control.reset()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showNumber(num1)
+    num1 += 1
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(num2)
+    num2 += 1
+})
+let num2 = 0
+let num1 = 0
+basic.showIcon(IconNames.Happy)
+num1 = randint(1, 100)
+num2 = randint(1, 100)
